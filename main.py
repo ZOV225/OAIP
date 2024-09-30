c5c819a4373ca6aa2de519e62f96de7955c39c61
ZOV225 <vanokarpenko123ps@gmail.com>
2024-09-30 12:41:56 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-z, x, c = input(), input(), input()
-print("билет на",z, "в", x, "на", c, "забронирован")
+#a = input()
+#if a == "Python":
+# print("ДА")
+#else:
+# print("НЕТ")
+
+#zxc = input()
+#zxc2 = input()
+#if zxc in "да" and zxc2 in "да":
+ #   print("верно")
+#elif zxc in "нет" and zxc2 in "нет":
+ #  print("верно")
+#else:
+#    print("неверно")
+
+#zov = input()
+#zov2 = input()
+#zov3 = input()
+#if zov == "1" and zov2 == "2" and zov3 == "3":
+#   print("ГОРИ")
+#elif zov == "раз" and zov2 == "два" and zov3 == "три":
+#   print("ГОРИ")
+#else: print("не гори")
