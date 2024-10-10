2e6af782bc813e706a2468f8902837b27fc35db8
ZOV225 <vanokarpenko123ps@gmail.com>
2024-10-10 12:35:25 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -21,3 +21,12 @@
 #elif zov == "раз" and zov2 == "два" and zov3 == "три":
 #   print("ГОРИ")
 #else: print("не гори")
+
+#city_july = input()
+#city_august = input()
+#b = "Тула"
+#v = "Пенза"
+#if city_july != city_august and (city_july == b and city_august != b or city_august == v and city_july != b):
+    #print('ДА')
+#else:
+    #print('НЕТ')
