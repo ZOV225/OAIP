3be90884b2bdb56dd508c6b7f90c5ef082f5100d
ZOV225 <vanokarpenko123ps@gmail.com>
2024-10-31 12:46:15 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -7,11 +7,11 @@
 #zxc = input()
 #zxc2 = input()
 #if zxc in "да" and zxc2 in "да":
- #   print("верно")
+#  print("верно")
 #elif zxc in "нет" and zxc2 in "нет":
- #  print("верно")
+#  print("верно")
 #else:
-#    print("неверно")
+#   print("неверно")
 
 #zov = input()
 #zov2 = input()
@@ -22,11 +22,25 @@
 #   print("ГОРИ")
 #else: print("не гори")
 
-#city_july = input()
-#city_august = input()
-#b = "Тула"
-#v = "Пенза"
-#if city_july != city_august and (city_july == b and city_august != b or city_august == v and city_july != b):
-    #print('ДА')
-#else:
-    #print('НЕТ')
+#category = input("Введите категорию товара: ")
+
+#if category.lower() == "продукты":
+#    price = int(input("Введите цену товара: "))
+#    if price < 100:
+#        print("Попробуйте нашу выпечку!")
+#    elif price>= 100 and price <= 500:
+#       print("Как насчёт орехов в шоколаде?")
+#    else:
+#        print("Попробуйте экзотические фрукты!")
+#    print("Загляните в товары для дома!")
+
+price1 = int(input('Цена первого товара:'))
+price2 = int(input('Цена второго товара:'))
+price3 = int(input('Цена третьего товара:'))
+if price1 <= price2 and price2 <= price3:
+    print('Акция!')
+    print('К оплате:', (price1 + price2 + price3)/2)
+elif price3 <= price2 and price2 <= price1:
+    print('Акция!')
+    print('К оплате:', (price1 + price2 + price3)/3)
+else: print('К оплате:', price1 + price2 + price3)
